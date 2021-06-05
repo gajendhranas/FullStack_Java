537270fdd800790893c3e7e7c931126860198ab3
Gajendhran Sekar <gajendhranas@gmail.com>
2021-06-05 15:58:59 +0530

    Day1Training

---
+++ HelloWorldArg.java
@@ -0,0 +1,8 @@
+public class HelloWorldArg{
+	public static void main(String[] args){
+		System.out.println("Hello World!!");
+		System.out.println(args.length);
+		System.out.println(args[1] +" "+ args[0]);
+
+	}
+}
